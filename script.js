ef672e8d74433e75062068303f08fdeeb2db986c
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-29 23:45:03 -0300

    att

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const imgs= document.getElementById("img");
-const img = document.querySelector("#img img");
-
-let idx= 0 ;
-
-function carrossel(){
- idx++;
-
- if(idx > img.length-1){
-    idx =0;
- }
-
- imgs.style.transform =  `translateX(${-idx * 500}px)`;
-}
-
-setInterval(carrossel, 1800)
+ 
